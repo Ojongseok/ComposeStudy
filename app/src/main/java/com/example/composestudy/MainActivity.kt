491c25a7a255f6a172d0d72da25521b45f43b529
Ojongseok <whdtjr6889@naver.com>
2023-12-10 02:35:49 +0900

    [Ch 03.12] Compose 컴포넌트: Card

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -28,6 +28,9 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -35,8 +38,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,44 +50,82 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.max
 import coil.compose.AsyncImage
 import coil.compose.rememberImagePainter
+import com.example.composestudy.MainActivity.Companion.data
 import com.example.composestudy.ui.theme.ComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
+    companion object {
+        val data = CardData(
+            imageUrl = "https://i.namu.wiki/i/8s6Kl-iISoyRmIXCaDtEmvHb2WKT9hDAudqp6g7Ln87xF44m0TVDnlCx5kFQw-IQVPxwi-0swj11ZofLhvKx0-w9uLY_t6jTOeHl2fTq9T69f6XDRh8jIsz_YrT2UxTP-xNS-rhIUJsjQ3Gk4APlwA.webp",
+            imageDescription = "그랜드 캐년",
+            author = "오종석",
+            description = "미국 애리조나 주 북서부 고원지대가 콜로라도 강에 침식되어 생긴 협곡이다. 미국하면 떠오르는 유명한 관광지 중 한 곳이다."
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             ComposeStudyTheme {
-                CoilEx()
+                CardEx(Modifier, data)
             }
         }
     }
 }
 
 @Composable
-fun CoilEx() {
-    val imageUrl = "https://i.namu.wiki/i/8s6Kl-iISoyRmIXCaDtEmvHb2WKT9hDAudqp6g7Ln87xF44m0TVDnlCx5kFQw-IQVPxwi-0swj11ZofLhvKx0-w9uLY_t6jTOeHl2fTq9T69f6XDRh8jIsz_YrT2UxTP-xNS-rhIUJsjQ3Gk4APlwA.webp"
-    val painter = rememberImagePainter(data = imageUrl)
-
-    Column {
-//        Image(
-//            painter = painter,
-//            modifier = Modifier.size(160.dp),
-//            contentDescription = "그랜드 캐년"
-//        )
-        AsyncImage(
-            model = imageUrl,
-            contentDescription = "그랜드 캐년",
-            modifier = Modifier.padding(top = 20.dp)
+fun CardEx(modifier: Modifier = Modifier, cardData: CardData) {
+    Card(
+        modifier = modifier.padding(8.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 8.dp
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Green
         )
-    }
 
+    ) {
+        Row(
+            modifier = modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            AsyncImage(
+                modifier = modifier
+                    .size(48.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.FillBounds,
+                model = cardData.imageUrl,
+                placeholder = ColorPainter(Color.Gray),
+                contentDescription = cardData.imageDescription
+            )
+
+            Spacer(modifier = modifier.size(8.dp))
+
+            Column {
+                Text(
+                    text = cardData.author
+                )
+                Spacer(modifier = modifier.size(4.dp))
+                Text(
+                    text = cardData.description
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeStudyTheme {
-        CoilEx()
+        CardEx(cardData = data)
     }
 }
+
+data class CardData(
+    val imageUrl: String,
+    val imageDescription: String,
+    val author: String,
+    val description: String
+)
