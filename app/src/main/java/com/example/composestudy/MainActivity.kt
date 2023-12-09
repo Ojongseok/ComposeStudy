c71f3df26d4035695827bb4dd4e63073d0050c68
Ojongseok <whdtjr6889@naver.com>
2023-12-09 23:38:52 +0900

    [Ch 03.04] Compose 컴포넌트: Modifier

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -1,37 +1,26 @@
 package com.example.composestudy
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.composestudy.ui.theme.ComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,37 +29,57 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ComposeStudyTheme {
-                ButtonExample(
-                    onButtonClicked = {
-                        Toast.makeText(this, "터치터치", Toast.LENGTH_SHORT).show()
-                    }
-                )
+                modifierEx()
             }
         }
     }
 }
 
 @Composable
-fun ButtonExample(
-    onButtonClicked: () -> Unit
-) {
+fun modifierEx() {
     Button(
-        onClick = onButtonClicked,
-        enabled = true,
-//        border = BorderStroke(4.dp, Color.Magenta),
-        shape = RoundedCornerShape(12.dp),
-        contentPadding = PaddingValues(16.dp, 4.dp)
+        modifier = Modifier
+            .padding(10.dp),
+        shape = RoundedCornerShape(20.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Magenta
+        ),
+        onClick = {}
     ) {
         Icon(
-            imageVector = Icons.Filled.Send,
-            contentDescription = "Send"
+            imageVector = Icons.Default.Search,
+            contentDescription = null
         )
         Spacer(
             modifier = Modifier.size(ButtonDefaults.IconSpacing)
         )
-        Text(text = "Send")
+        Text(
+            text = "Search"
+        )
     }
 }
+
+//@Composable
+//fun ButtonExample(
+//    onButtonClicked: () -> Unit
+//) {
+//    Button(
+//        onClick = onButtonClicked,
+//        enabled = true,
+////        border = BorderStroke(4.dp, Color.Magenta),
+//        shape = RoundedCornerShape(12.dp),
+//        contentPadding = PaddingValues(16.dp, 4.dp)
+//    ) {
+//        Icon(
+//            imageVector = Icons.Filled.Send,
+//            contentDescription = "Send"
+//        )
+//        Spacer(
+//            modifier = Modifier.size(ButtonDefaults.IconSpacing)
+//        )
+//        Text(text = "Send")
+//    }
+//}
 
 //@Composable
 //fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -93,6 +102,6 @@ fun ButtonExample(
 @Composable
 fun GreetingPreview() {
     ComposeStudyTheme {
-        ButtonExample(onButtonClicked = {})
+        modifierEx()
     }
 }
