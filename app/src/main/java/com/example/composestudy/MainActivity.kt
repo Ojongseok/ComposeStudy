0c9939c4dd0e4c4ac121ba0db910eac5d06ccc47
Ojongseok <whdtjr6889@naver.com>
2023-12-10 01:56:24 +0900

    [Ch 03.09] Compose 컴포넌트: BoxWithConstraints

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -7,15 +7,18 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -34,6 +37,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import com.example.composestudy.ui.theme.ComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,66 +46,44 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ComposeStudyTheme {
-                ColumnEx()
+                Outer()
             }
         }
     }
 }
 
 @Composable
-fun ColumnEx() {
-    Column(
-        modifier = Modifier.size(120.dp),
-        horizontalAlignment = Alignment.End
-    ) {
-        Text(
-            text = "첫번째!",
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        )
-        Text(text = "두번째!")
-        Text(
-            text = "세번째!",
-            modifier = Modifier.align(Alignment.Start)
+fun Outer() {
+    Column {
+        Inner(
+            Modifier
+            .widthIn(min = 100.dp, max = 350.dp)
+            .heightIn(min = 20.dp, max = 200.dp)
         )
     }
 }
 
-//@Composable
-//fun RowEx() {
-//    Row(
-//        modifier = Modifier.size(200.dp, 40.dp),
-//        verticalAlignment = Alignment.Bottom,
-//        horizontalArrangement = Arrangement.SpaceBetween
-//    ) {
-//        Text(
-//            text = "첫번째 !!",
-//            modifier = Modifier
-//                .align(Alignment.Top)
-//                .weight(2f)
-//                .background(Color.Magenta),
-//            textAlign = TextAlign.End
-//        )
-//        Icon(
-//            modifier = Modifier
-//                .weight(1f)
-//                .background(Color.Cyan),
-//            imageVector = Icons.Filled.Add,
-//            contentDescription = null
-//        )
-//        Text(
-//            modifier = Modifier
-//                .weight(3f)
-//                .background(Color.Yellow),
-//            text = "세번째 !!",
-//            textAlign = TextAlign.Center
-//        )
-//    }
-//}
+@Composable
+fun Inner(modifier: Modifier = Modifier) {
+    BoxWithConstraints(
+        modifier = modifier
+    ) {
+        if (maxHeight > 100.dp) {
+            Text(
+                text = "여기 꽤 길군요 ?",
+                modifier = modifier.align(Alignment.BottomCenter)
+            )
+        }
+        Text(
+            text = "maxW: $maxWidth maxH: $maxHeight minW: $minWidth minH: $minHeight"
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     ComposeStudyTheme {
-        ColumnEx()
+        Outer()
     }
 }
