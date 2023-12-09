d47a62a2a7b792cbafb6764efaa9184ff377f645
Ojongseok <whdtjr6889@naver.com>
2023-12-10 00:27:36 +0900

    [Ch 03.07] Compose 컴포넌트: Rox

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -5,9 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -15,6 +18,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -37,63 +41,48 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ComposeStudyTheme {
-                BoxEx()
+                RowEx()
             }
         }
     }
 }
 
 @Composable
-fun BoxEx() {
-    Box {
-        Box(modifier = Modifier.fillMaxSize().background(Color.Cyan).align(Alignment.CenterStart))
-        Box(modifier = Modifier.size(70.dp).background(Color.Yellow).align(Alignment.Center))
+fun RowEx() {
+    Row(
+        modifier = Modifier.size(200.dp, 40.dp),
+        verticalAlignment = Alignment.Bottom,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = "첫번째 !!",
+            modifier = Modifier
+                .align(Alignment.Top)
+                .weight(2f)
+                .background(Color.Magenta),
+            textAlign = TextAlign.End
+        )
+        Icon(
+            modifier = Modifier
+                .weight(1f)
+                .background(Color.Cyan),
+            imageVector = Icons.Filled.Add,
+            contentDescription = null
+        )
+        Text(
+            modifier = Modifier
+                .weight(3f)
+                .background(Color.Yellow),
+            text = "세번째 !!",
+            textAlign = TextAlign.Center
+        )
     }
 }
-
-//@Composable
-//fun ButtonExample(
-//    onButtonClicked: () -> Unit
-//) {
-//    Button(
-//        onClick = onButtonClicked,
-//        enabled = true,
-////        border = BorderStroke(4.dp, Color.Magenta),
-//        shape = RoundedCornerShape(12.dp),
-//        contentPadding = PaddingValues(16.dp, 4.dp)
-//    ) {
-//        Icon(
-//            imageVector = Icons.Filled.Send,
-//            contentDescription = "Send"
-//        )
-//        Spacer(
-//            modifier = Modifier.size(ButtonDefaults.IconSpacing)
-//        )
-//        Text(text = "Send")
-//    }
-//}
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        modifier = Modifier
-//            .width(300.dp)
-//            .height(200.dp),
-//        text = "Helloooooo ${name}!\nHello ${name}!\nHello ${name}!",
-//        color = Color.Blue,
-//        fontSize = 24.sp,
-//        fontWeight = FontWeight.Bold,
-//        fontFamily = FontFamily.SansSerif,
-//        maxLines = 2,
-//        textDecoration = TextDecoration.Underline,
-//        textAlign = TextAlign.Center
-//    )
-//}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeStudyTheme {
-        BoxEx()
+        RowEx()
     }
 }
