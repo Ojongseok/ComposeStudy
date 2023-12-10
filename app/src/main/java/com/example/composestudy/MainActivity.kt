b93003cd553de08b7fc52dd3847c8272d9de6216
Ojongseok <whdtjr6889@naver.com>
2023-12-10 17:04:32 +0900

    [Ch 03.13] Compose 컴포넌트: Checkbox

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.composestudy
 
 import android.os.Bundle
+import android.widget.CheckBox
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
@@ -31,11 +33,16 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -50,68 +57,43 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.max
 import coil.compose.AsyncImage
 import coil.compose.rememberImagePainter
-import com.example.composestudy.MainActivity.Companion.data
 import com.example.composestudy.ui.theme.ComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
-    companion object {
-        val data = CardData(
-            imageUrl = "https://i.namu.wiki/i/8s6Kl-iISoyRmIXCaDtEmvHb2WKT9hDAudqp6g7Ln87xF44m0TVDnlCx5kFQw-IQVPxwi-0swj11ZofLhvKx0-w9uLY_t6jTOeHl2fTq9T69f6XDRh8jIsz_YrT2UxTP-xNS-rhIUJsjQ3Gk4APlwA.webp",
-            imageDescription = "그랜드 캐년",
-            author = "오종석",
-            description = "미국 애리조나 주 북서부 고원지대가 콜로라도 강에 침식되어 생긴 협곡이다. 미국하면 떠오르는 유명한 관광지 중 한 곳이다."
-        )
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             ComposeStudyTheme {
-                CardEx(Modifier, data)
+                CheckBoxEx(Modifier)
             }
         }
     }
 }
 
 @Composable
-fun CardEx(modifier: Modifier = Modifier, cardData: CardData) {
-    Card(
-        modifier = modifier.padding(8.dp),
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 8.dp
-        ),
-        colors = CardDefaults.cardColors(
-            containerColor = Color.Green
+fun CheckBoxEx(modifier: Modifier = Modifier) {
+    val (getter, setter) = remember { mutableStateOf(false) }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.clickable {
+            setter(!getter)
+        }
+    ) {
+//        var isChecked = remember  { mutableStateOf(false) }
+//        var isChecked by remember { mutableStateOf(false) }
+
+        Checkbox(
+            checked = getter,
+            onCheckedChange =  {
+                setter(it)
+            }
         )
 
-    ) {
-        Row(
-            modifier = modifier.padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            AsyncImage(
-                modifier = modifier
-                    .size(48.dp)
-                    .clip(CircleShape),
-                contentScale = ContentScale.FillBounds,
-                model = cardData.imageUrl,
-                placeholder = ColorPainter(Color.Gray),
-                contentDescription = cardData.imageDescription
-            )
-
-            Spacer(modifier = modifier.size(8.dp))
-
-            Column {
-                Text(
-                    text = cardData.author
-                )
-                Spacer(modifier = modifier.size(4.dp))
-                Text(
-                    text = cardData.description
-                )
-            }
-        }
+        Text(
+            text = "당신은 개발자입니까?"
+        )
     }
 }
 
@@ -119,13 +101,6 @@ fun CardEx(modifier: Modifier = Modifier, cardData: CardData) {
 @Composable
 fun DefaultPreview() {
     ComposeStudyTheme {
-        CardEx(cardData = data)
+        CheckBoxEx()
     }
 }
-
-data class CardData(
-    val imageUrl: String,
-    val imageDescription: String,
-    val author: String,
-    val description: String
-)
