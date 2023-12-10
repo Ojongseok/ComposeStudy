21be6c5f2c01acb193bacf5075eef1586c6efb8e
Ojongseok <whdtjr6889@naver.com>
2023-12-10 21:21:56 +0900

    [Ch 03.16] Compose 컴포넌트: Slot API

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -1,69 +1,23 @@
 package com.example.composestudy
 
 import android.os.Bundle
-import android.widget.CheckBox
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
-import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
-import coil.compose.AsyncImage
-import coil.compose.rememberImagePainter
 import com.example.composestudy.ui.theme.ComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -72,79 +26,64 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ComposeStudyTheme {
-                TopBarEx("jongseok")
+                SlotEx()
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBarEx(name: String) {
+fun SlotEx() {
+    var checked1 by remember { mutableStateOf(false) }
+    var checked2 by remember { mutableStateOf(false) }
+
     Column {
-//        TopAppBar(
-        CenterAlignedTopAppBar(
-            title = { Text(text = "TopAppBar") },
-            navigationIcon = {
-                IconButton(onClick = {}) {
-                    Icon(
-                        imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = null
-                    )
-                }
-            },
-            actions = {
-                IconButton(onClick = {}) {
-                    Icon(
-                        imageVector = Icons.Filled.Search,
-                        contentDescription = "검색"
-                    )
-                }
-//                IconButton(onClick = {}) {
-//                    Icon(
-//                        imageVector = Icons.Filled.Settings,
-//                        contentDescription = "설정"
-//                    )
-//                }
+        CheckboxWithSlot(
+            checked = checked1,
+            onCheckedChanged = {
+                checked1 = !checked1
             }
-        )
-        
-        Text(text = "Hello $name")
+        ) {
+            Text(text = "텍스트1", modifier = Modifier.align(Alignment.CenterVertically))
+        }
+
+        CheckboxWithSlot(
+            checked = checked2,
+            onCheckedChanged = {
+                checked2 = !checked2
+            }
+        ) {
+            Text(text = "텍스트2", modifier = Modifier.align(Alignment.CenterVertically))
+        }
     }
 }
 
-//@OptIn(ExperimentalMaterial3Api::class)
-//@Composable
-//fun TextFieldEx() {
-//    var name by remember {
-//        mutableStateOf("")
-//    }
-//
-//    Column(
-//        modifier = Modifier.padding(16.dp)
-//    ) {
-//        OutlinedTextField(
-//            value = name,
-//            label = {
-//                Text(text = "이름")
-//            },
-//            onValueChange = {
-//                name = it
-//            },
-//            placeholder = {
-//                Text(text = "이름을 입력하세요")
-//            }
-//        )
-//        Spacer(modifier = Modifier.size(8.dp))
-//
-//        Text(text = "Hello $name")
-//    }
-//}
+@Composable
+fun CheckboxWithSlot(
+    checked: Boolean,
+    onCheckedChanged: () -> Unit,
+    content: @Composable RowScope.() -> Unit
+) {
+    Row(
+        modifier = Modifier.clickable {
+            onCheckedChanged()
+        },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = {
+                onCheckedChanged()
+            }
+        )
+        content()
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeStudyTheme {
-        TopBarEx("jongseok")
+        SlotEx()
     }
 }
