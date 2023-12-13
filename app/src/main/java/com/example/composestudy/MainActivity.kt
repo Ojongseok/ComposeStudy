06d8cc020d2313b83cea38014ce2f5e726cd826e
Ojongseok <whdtjr6889@naver.com>
2023-12-13 16:26:14 +0900

    [Ch 03.18] Compose 컴포넌트: 카탈로그 Card 구현

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -4,18 +4,24 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -29,6 +35,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composestudy.ui.theme.ComposeStudyTheme
@@ -39,124 +48,63 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ComposeStudyTheme {
-                ScaffoldEx()
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun ScaffoldEx() {
-    var checked by remember { mutableStateOf(false) }
-
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                navigationIcon = {
-                    IconButton(onClick = {}) {
-                        Image(
-                            imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = null
-                        )
-                    }
-                },
-                title = {
-                    Text(text = "Scaffold App")
-                }
-            )
-        }
+fun Item(itemData: ItemData) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        modifier = Modifier.padding(16.dp)
     ) {
-        Surface(
+        Column(
             modifier = Modifier.padding(8.dp)
         ) {
-            CheckboxWithContent(
-                checked = checked,
-                toggleState = {
-                    checked = !checked
-                }
-            ) {
-                Text(text = "컴포즈를 좋아해요")
-            }
+            Image(
+                painter = painterResource(id = itemData.imageId),
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(
+                text = itemData.title,
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.size(4.dp))
+            Text(
+                text = itemData.description,
+                style = MaterialTheme.typography.bodyMedium,
+                fontWeight = FontWeight.Light
+            )
         }
     }
 }
 
+data class ItemData(
+    @DrawableRes val imageId: Int,
+    val title: String,
+    val description: String
+)
+
+@Preview(showBackground = true)
 @Composable
-fun CheckboxWithContent(
-    checked: Boolean,
-    toggleState: () -> Unit,
-    content: @Composable RowScope.() -> Unit
-) {
-    Row(
-        modifier = Modifier.clickable {
-            toggleState()
-        },
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Checkbox(
-            checked = checked,
-            onCheckedChange = {
-                toggleState()
-            }
+fun ItemPreview() {
+    ComposeStudyTheme {
+        Item(
+            ItemData(
+                imageId = R.drawable.image_grand_canyon,
+                title = "해변 놀이 공원",
+                description = "해변 놀이 공원 설명입니다. 해변 놀아놀이놀이 설명이야 루루룰ㄹ룰ㄹ\n해변 놀이 공원 설명입니다. 해변 놀아놀이놀이 설명이야 루루룰ㄹ룰ㄹ"
+            )
         )
-        content()
     }
 }
-
-//@Composable
-//fun SlotEx() {
-//    var checked1 by remember { mutableStateOf(false) }
-//    var checked2 by remember { mutableStateOf(false) }
-//
-//    Column {
-//        CheckboxWithSlot(
-//            checked = checked1,
-//            onCheckedChanged = {
-//                checked1 = !checked1
-//            }
-//        ) {
-//            Text(text = "텍스트1", modifier = Modifier.align(Alignment.CenterVertically))
-//        }
-//
-//        CheckboxWithSlot(
-//            checked = checked2,
-//            onCheckedChanged = {
-//                checked2 = !checked2
-//            }
-//        ) {
-//            Text(text = "텍스트2", modifier = Modifier.align(Alignment.CenterVertically))
-//        }
-//    }
-//}
-//
-//@Composable
-//fun CheckboxWithSlot(
-//    checked: Boolean,
-//    onCheckedChanged: () -> Unit,
-//    content: @Composable RowScope.() -> Unit
-//) {
-//    Row(
-//        modifier = Modifier.clickable {
-//            onCheckedChanged()
-//        },
-//        verticalAlignment = Alignment.CenterVertically
-//    ) {
-//        Checkbox(
-//            checked = checked,
-//            onCheckedChange = {
-//                onCheckedChanged()
-//            }
-//        )
-//        content()
-//    }
-//}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeStudyTheme {
-        ScaffoldEx()
     }
 }
