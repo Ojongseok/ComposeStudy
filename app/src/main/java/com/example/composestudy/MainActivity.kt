6c09ce275feaf73abb1ce2ba43e19f4f3dfbe927
Ojongseok <whdtjr6889@naver.com>
2023-12-13 16:08:45 +0900

    [Ch 03.17] Compose 컴포넌트: Scaffold

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.example.composestudy
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -17,7 +28,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composestudy.ui.theme.ComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,64 +39,124 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ComposeStudyTheme {
-                SlotEx()
+                ScaffoldEx()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun ScaffoldEx() {
+    var checked by remember { mutableStateOf(false) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Image(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = null
+                        )
+                    }
+                },
+                title = {
+                    Text(text = "Scaffold App")
+                }
+            )
+        }
+    ) {
+        Surface(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            CheckboxWithContent(
+                checked = checked,
+                toggleState = {
+                    checked = !checked
+                }
+            ) {
+                Text(text = "컴포즈를 좋아해요")
             }
         }
     }
 }
 
 @Composable
-fun SlotEx() {
-    var checked1 by remember { mutableStateOf(false) }
-    var checked2 by remember { mutableStateOf(false) }
-
-    Column {
-        CheckboxWithSlot(
-            checked = checked1,
-            onCheckedChanged = {
-                checked1 = !checked1
-            }
-        ) {
-            Text(text = "텍스트1", modifier = Modifier.align(Alignment.CenterVertically))
-        }
-
-        CheckboxWithSlot(
-            checked = checked2,
-            onCheckedChanged = {
-                checked2 = !checked2
-            }
-        ) {
-            Text(text = "텍스트2", modifier = Modifier.align(Alignment.CenterVertically))
-        }
-    }
-}
-
-@Composable
-fun CheckboxWithSlot(
+fun CheckboxWithContent(
     checked: Boolean,
-    onCheckedChanged: () -> Unit,
+    toggleState: () -> Unit,
     content: @Composable RowScope.() -> Unit
 ) {
     Row(
         modifier = Modifier.clickable {
-            onCheckedChanged()
+            toggleState()
         },
         verticalAlignment = Alignment.CenterVertically
     ) {
         Checkbox(
             checked = checked,
             onCheckedChange = {
-                onCheckedChanged()
+                toggleState()
             }
         )
         content()
     }
 }
 
+//@Composable
+//fun SlotEx() {
+//    var checked1 by remember { mutableStateOf(false) }
+//    var checked2 by remember { mutableStateOf(false) }
+//
+//    Column {
+//        CheckboxWithSlot(
+//            checked = checked1,
+//            onCheckedChanged = {
+//                checked1 = !checked1
+//            }
+//        ) {
+//            Text(text = "텍스트1", modifier = Modifier.align(Alignment.CenterVertically))
+//        }
+//
+//        CheckboxWithSlot(
+//            checked = checked2,
+//            onCheckedChanged = {
+//                checked2 = !checked2
+//            }
+//        ) {
+//            Text(text = "텍스트2", modifier = Modifier.align(Alignment.CenterVertically))
+//        }
+//    }
+//}
+//
+//@Composable
+//fun CheckboxWithSlot(
+//    checked: Boolean,
+//    onCheckedChanged: () -> Unit,
+//    content: @Composable RowScope.() -> Unit
+//) {
+//    Row(
+//        modifier = Modifier.clickable {
+//            onCheckedChanged()
+//        },
+//        verticalAlignment = Alignment.CenterVertically
+//    ) {
+//        Checkbox(
+//            checked = checked,
+//            onCheckedChange = {
+//                onCheckedChanged()
+//            }
+//        )
+//        content()
+//    }
+//}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeStudyTheme {
-        SlotEx()
+        ScaffoldEx()
     }
 }
