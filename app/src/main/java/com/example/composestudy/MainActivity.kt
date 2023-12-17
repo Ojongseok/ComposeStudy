8f2ef18a37b024c55059714f359f20d260d7e877
Ojongseok <whdtjr6889@naver.com>
2023-12-17 17:27:00 +0900

    [Ch 04.08] Compose 컴포넌트: Alert Dialog

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -4,17 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -41,7 +48,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CanvasEx()
+                    DialogEx()
                 }
             }
         }
@@ -49,17 +56,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CanvasEx() {
-    Canvas(
-        modifier = Modifier.size(100.dp)
-    ) {
-        drawLine(Color.Red, Offset(20f, 10f), Offset(35f, 77f))
+fun DialogEx() {
+    var openDialog by remember { mutableStateOf(false) }
+    var counter by remember { mutableStateOf(0) }
 
-        drawCircle(Color.Yellow, 30f, Offset(40f, 40f))
+    Column {
+        Button(onClick = {
+            openDialog = true
+        }) {
+            Text(text = "다이어로그 열기")
+        }
+        Text(text = "카운터 : $counter")
+    }
 
-        drawRect(Color.Magenta, Offset(30f,30f), Size(10f,10f))
-
-
+    if (openDialog) {
+        AlertDialog(
+            onDismissRequest = {
+                openDialog = false
+            },
+            confirmButton = {
+                Button(onClick = {
+                    counter++
+                    openDialog = false
+                }) {
+                    Text(text = "더하기")
+                }
+            },
+            dismissButton = {
+                Button(onClick = { openDialog = false }) {
+                    Text(text = "취소")
+                }
+            },
+            title = {
+                Text(text = "더하기 타이틀")
+            },
+            text = {
+                Text(text = "더하기 버튼을 누르면 counter가 증가합니다.")
+            }
+        )
     }
 }
 
@@ -68,6 +102,6 @@ fun CanvasEx() {
 @Composable
 fun DefaultPreview() {
     ComposeStudyTheme {
-        CanvasEx()
+        DialogEx()
     }
 }
