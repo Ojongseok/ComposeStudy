27a7584a2729dc3f2133338b804c45ae3afdbd50
Ojongseok <whdtjr6889@naver.com>
2023-12-23 21:56:26 +0900

    [Ch 04.12] Compose 컴포넌트: BottomAppBar

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -8,14 +8,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -31,6 +34,7 @@ import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -65,56 +69,69 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SnackbarEx()
+                    BottomAppBarEx()
                 }
             }
         }
     }
 }
 
-@ExperimentalMaterial3Api
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun SnackbarEx() {
+fun BottomAppBarEx() {
     val snackbarHostState = remember { SnackbarHostState() }
-    val scope = rememberCoroutineScope()
+    val coroutineScope = rememberCoroutineScope()
     var counter by remember { mutableStateOf(0) }
 
+    // 버튼의 onClick에는 suspend fun을 실행할 수 있지만 밖에서 호출할 때는 LaunchedEffect같이 코루틴 처리
+    LaunchedEffect(snackbarHostState) {
+        snackbarHostState.showSnackbar("밖에서 호출")
+    }
+
     Scaffold(
-        snackbarHost = {
-            SnackbarHost(snackbarHostState)
-        },
-        content = {
-            Button(onClick = {
-                counter++
-                scope.launch {
-                    val result = snackbarHostState.showSnackbar(
-                        message = "카운터는 ${counter}입니다.",
-                        actionLabel = "닫기",
-                        duration = SnackbarDuration.Short
-                    )
-
-                    when(result) {
-                        SnackbarResult.ActionPerformed -> {
-
+        snackbarHost = { SnackbarHost(snackbarHostState)},
+        bottomBar = {
+            BottomAppBar {
+                Row {
+                    Text(text = "헬로")
+                    Button(onClick = {
+                        coroutineScope.launch {
+                            snackbarHostState.showSnackbar("안녕하세요.")
                         }
-                        SnackbarResult.Dismissed -> {
-
-                        }
+                    }) {
+                        Text(text = "인사하기")
+                    }
+                    Button(onClick = {
+                        counter++
+                    }) {
+                        Text(text = "더하기")
+                    }
+                    Button(onClick = {
+                        counter--
+                    }) {
+                        Text(text = "빼기")
                     }
                 }
-            }) {
-                Text(text = "더하기")
             }
         }
-    )
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                text = "카운터는 ${counter}회입니다."
+            )
+        }
+    }
 }
+
 
 @ExperimentalMaterial3Api
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeStudyTheme {
-        SnackbarEx()
+        BottomAppBarEx()
     }
 }
