22b514c879f73057cbf11e45ff68e7b70a0c12dd
Ojongseok <whdtjr6889@naver.com>
2023-12-24 20:13:28 +0900

    [Ch 04.14] Compose 컴포넌트: 애니메이션 (1)

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -6,7 +6,17 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,6 +26,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.BottomAppBar
@@ -27,6 +38,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
@@ -45,6 +57,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -71,97 +84,124 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    PyeongToSquareMeter()
+                    AnimationEx()
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun PyeongToSquareMeter() {
-    var pyeong by rememberSaveable { mutableStateOf("") }
-    var squareMeter by rememberSaveable { mutableStateOf("") }
-//    Column(
-//        modifier = Modifier.padding(16.dp)
-//    ) {
-//        OutlinedTextField(
-//            value = pyeong,
-//            onValueChange = {
-//                if (it.isBlank()) {
-//                    pyeong = ""
-//                }
-//                val numericValue = it.toFloatOrNull() ?: return@OutlinedTextField
-//                pyeong = it
-//
-//                squareMeter = (numericValue*3.306).toString()
-//            },
-//            label = {
-//                Text(text = "평")
-//            }
-//        )
-//        OutlinedTextField(
-//            value = squareMeter,
-//            onValueChange = {
-//
-//            },
-//            label = {
-//                Text(text = "제곱미터")
-//            }
-//        )
-//    }
+fun AnimationEx() {
+    var helloWorldVisible by remember { mutableStateOf(true) }
+    var isRed by remember { mutableStateOf(false) }
 
-    PyeongToSquareMeterStateless(
-        pyeong = pyeong,
-        squareMeter = squareMeter,
-        onPyeongChange = {
-            if (it.isBlank()) {
-                pyeong = ""
-                squareMeter = ""
-                return@PyeongToSquareMeterStateless
-            }
-            val numericValue = it.toFloatOrNull() ?: return@PyeongToSquareMeterStateless
-            pyeong = it
-
-            squareMeter = (numericValue*3.306).toString()
-        }
+    // State에 따라 Red부터 White로 변하는 상태를 보관하는 용도
+    val backgroundColor by animateColorAsState(
+        targetValue = if (isRed) Color.Red else Color.White, label = ""
     )
-}
 
-// Step2 -> 상태를 관리하는 범위를 좁히기 위한 State 호스팅 방식으로 작성
-@Composable
-fun PyeongToSquareMeterStateless(
-    pyeong: String,
-    squareMeter: String,
-    onPyeongChange: (String) -> Unit
-) {
+    // State에 따라 0.5 ~ 1.0 사이의 수치를 모두 가짐
+    val alpha by animateFloatAsState(
+        targetValue = if (isRed) 1.0f else 0.5f, label = ""
+    )
+
     Column(
-        modifier = Modifier.padding(16.dp)
+        modifier = Modifier
+            .padding(16.dp)
+            .background(backgroundColor)
+            .alpha(alpha)
     ) {
-        OutlinedTextField(
-            value = pyeong,
-            onValueChange = onPyeongChange,
-            label = {
-                Text(text = "평")
-            }
-        )
-        OutlinedTextField(
-            value = squareMeter,
-            onValueChange = { },
-            label = {
-                Text(text = "제곱미터")
-            }
-        )
+        AnimatedVisibility(
+            visible = helloWorldVisible,
+            enter = slideInVertically() + expandHorizontally()
+        ) {
+            Text(text = "Hello World!")
+        }
+
+        Row(
+            modifier = Modifier.selectable(
+                selected = helloWorldVisible,
+                onClick = {
+                    helloWorldVisible = true
+                }
+            ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = helloWorldVisible,
+                onClick = {
+                    helloWorldVisible = true
+                }
+            )
+            Text(text = "Hello World 보이기")
+        }
+
+        Row(
+            modifier = Modifier.selectable(
+                selected = !helloWorldVisible,
+                onClick = {
+                    helloWorldVisible = false
+                }
+            ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = !helloWorldVisible,
+                onClick = {
+                    helloWorldVisible = false
+                }
+            )
+            Text(text = "Hello World 감추기")
+        }
+
+
+        Text(text = "배경색을 바꾸어봅시다.")
+
+        Row(
+            modifier = Modifier.selectable(
+                selected = !isRed,
+                onClick = {
+                    isRed = false
+                }
+            ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = !isRed,
+                onClick = {
+                    isRed = false
+                }
+            )
+            Text(text = "흰색")
+        }
+
+        Row(
+            modifier = Modifier.selectable(
+                selected = isRed,
+                onClick = {
+                    isRed = true
+                }
+            ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = isRed,
+                onClick = {
+                    isRed = true
+                }
+            )
+            Text(text = "빨간색")
+        }
     }
 }
-
 
 @ExperimentalMaterial3Api
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeStudyTheme {
-        PyeongToSquareMeter()
+        AnimationEx()
     }
 }
