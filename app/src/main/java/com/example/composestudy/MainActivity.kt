879bd6d311ed74f7f75547c4131aef53a70fc31c
Ojongseok <whdtjr6889@naver.com>
2023-12-24 19:07:37 +0900

    [Ch 04.13] Compose 컴포넌트: State와 State Hoisting

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
@@ -39,6 +40,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -69,60 +71,88 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BottomAppBarEx()
+                    PyeongToSquareMeter()
                 }
             }
         }
     }
 }
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BottomAppBarEx() {
-    val snackbarHostState = remember { SnackbarHostState() }
-    val coroutineScope = rememberCoroutineScope()
-    var counter by remember { mutableStateOf(0) }
+fun PyeongToSquareMeter() {
+    var pyeong by rememberSaveable { mutableStateOf("") }
+    var squareMeter by rememberSaveable { mutableStateOf("") }
+//    Column(
+//        modifier = Modifier.padding(16.dp)
+//    ) {
+//        OutlinedTextField(
+//            value = pyeong,
+//            onValueChange = {
+//                if (it.isBlank()) {
+//                    pyeong = ""
+//                }
+//                val numericValue = it.toFloatOrNull() ?: return@OutlinedTextField
+//                pyeong = it
+//
+//                squareMeter = (numericValue*3.306).toString()
+//            },
+//            label = {
+//                Text(text = "평")
+//            }
+//        )
+//        OutlinedTextField(
+//            value = squareMeter,
+//            onValueChange = {
+//
+//            },
+//            label = {
+//                Text(text = "제곱미터")
+//            }
+//        )
+//    }
 
-    // 버튼의 onClick에는 suspend fun을 실행할 수 있지만 밖에서 호출할 때는 LaunchedEffect같이 코루틴 처리
-    LaunchedEffect(snackbarHostState) {
-        snackbarHostState.showSnackbar("밖에서 호출")
-    }
-
-    Scaffold(
-        snackbarHost = { SnackbarHost(snackbarHostState)},
-        bottomBar = {
-            BottomAppBar {
-                Row {
-                    Text(text = "헬로")
-                    Button(onClick = {
-                        coroutineScope.launch {
-                            snackbarHostState.showSnackbar("안녕하세요.")
-                        }
-                    }) {
-                        Text(text = "인사하기")
-                    }
-                    Button(onClick = {
-                        counter++
-                    }) {
-                        Text(text = "더하기")
-                    }
-                    Button(onClick = {
-                        counter--
-                    }) {
-                        Text(text = "빼기")
-                    }
-                }
+    PyeongToSquareMeterStateless(
+        pyeong = pyeong,
+        squareMeter = squareMeter,
+        onPyeongChange = {
+            if (it.isBlank()) {
+                pyeong = ""
+                squareMeter = ""
+                return@PyeongToSquareMeterStateless
             }
+            val numericValue = it.toFloatOrNull() ?: return@PyeongToSquareMeterStateless
+            pyeong = it
+
+            squareMeter = (numericValue*3.306).toString()
         }
+    )
+}
+
+// Step2 -> 상태를 관리하는 범위를 좁히기 위한 State 호스팅 방식으로 작성
+@Composable
+fun PyeongToSquareMeterStateless(
+    pyeong: String,
+    squareMeter: String,
+    onPyeongChange: (String) -> Unit
+) {
+    Column(
+        modifier = Modifier.padding(16.dp)
     ) {
-        Box(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Text(
-                modifier = Modifier.align(Alignment.Center),
-                text = "카운터는 ${counter}회입니다."
-            )
-        }
+        OutlinedTextField(
+            value = pyeong,
+            onValueChange = onPyeongChange,
+            label = {
+                Text(text = "평")
+            }
+        )
+        OutlinedTextField(
+            value = squareMeter,
+            onValueChange = { },
+            label = {
+                Text(text = "제곱미터")
+            }
+        )
     }
 }
 
@@ -132,6 +162,6 @@ fun BottomAppBarEx() {
 @Composable
 fun DefaultPreview() {
     ComposeStudyTheme {
-        BottomAppBarEx()
+        PyeongToSquareMeter()
     }
 }
