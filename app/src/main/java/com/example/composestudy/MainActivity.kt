1e94103845c393e9bb750f51e62bf3cb096f93eb
Ojongseok <whdtjr6889@naver.com>
2023-12-25 21:18:12 +0900

    [Ch 04.18] Compose 컴포넌트: ToDo 앱 구현

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -4,6 +4,7 @@ package com.example.composestudy
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
@@ -25,10 +26,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.CircleShape
@@ -37,6 +41,7 @@ import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -51,8 +56,10 @@ import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -68,12 +75,18 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import coil.compose.AsyncImage
 import com.example.composestudy.ui.theme.ComposeStudyTheme
 import kotlinx.coroutines.launch
@@ -88,133 +101,135 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AnimationEx()
+                    TopLevel()
                 }
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun AnimationEx() {
-    var isDarkMode by remember { mutableStateOf(false) }
+fun TopLevel() {
+    val (text, setText) = remember { mutableStateOf("") }
+    val toDoList = remember { mutableStateListOf<ToDoData>() }
 
-    val transition = updateTransition(targetState = isDarkMode, label = "다크모드 관리")
-
-    val backgroundColor by transition.animateColor(label = "") {
-        when(it) {
-            true -> Color.Black
-            false -> Color.White
+    val onSubmit: (String) -> Unit = {
+        val key = (toDoList.lastOrNull()?.key ?: 0) + 1
+        toDoList.add(ToDoData(key, text)).apply {
+            setText("")
         }
     }
 
-    val color by transition.animateColor(label = "") {
-        when(it) {
-            true -> Color.White
-            false -> Color.Black
+    // MutableStateList는 element의 추가, 삭제, 변경되었을 때만 상태변경을 인식해 ui가 갱신된다.
+    // 항목 하나의 값을 바꾸는 것보다 항목을 카피해서 인덱스에 다시 할당하는 방식으로 작성
+    val onToggle: (Int, Boolean) -> Unit = { key, checked ->
+        val i = toDoList.indexOfFirst {
+            it.key == key
         }
+        val newToDoList = toDoList[i].copy(done = checked)
+        toDoList[i] = newToDoList
     }
 
-    val alpha by transition.animateFloat(label = "") {
-        when(it) {
-            true -> 0.5f
-            false -> 1.0f
-        }
+    val onDelete: (Int) -> Unit = { key ->
+        val i = toDoList.indexOfFirst { it.key == key }
+        toDoList.removeAt(i)
     }
 
-    Column(
-        modifier = Modifier
-            .background(backgroundColor)
-            .alpha(alpha)
+    val onEdit: (Int, String) -> Unit = { key, text ->
+        val i = toDoList.indexOfFirst { it.key == key }
+        toDoList[i] = toDoList[i].copy(text = text)
+    }
+
+    Scaffold {
+        Column {
+            ToDoInput(
+                text = text,
+                onTextChange = setText,
+                onSubmit = onSubmit
+            )
+
+            LazyColumn {
+                items(toDoList, key = { it.key}) {
+                    Todo(
+                        toDoData = it,
+                        onToggle = onToggle,
+                        onDelete = onDelete,
+                        onEdit = onEdit
+                    )
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Todo(
+    toDoData: ToDoData,
+    onEdit: (key: Int, text: String) -> Unit = {_,_ ->},
+    onToggle: (key: Int, checked: Boolean) -> Unit = {_,_ ->},
+    onDelete: (key: Int) -> Unit = {}
+) {
+    var isEditing by remember { mutableStateOf(false) }
+
+    Card(
+        modifier = Modifier.padding(4.dp),
+        elevation = CardDefaults.elevatedCardElevation(8.dp)
     ) {
-        RadioButtonWithText(text = "일반 모드", color = color, selected = !isDarkMode) {
-            isDarkMode = false
-        }
-        RadioButtonWithText(text = "다크 모드", color = color, selected = isDarkMode) {
-            isDarkMode = true
-        }
-
-//        Row {
-//            Box(
-//                modifier = Modifier
-//                    .background(Color.Red)
-//                    .size(20.dp),
-//                contentAlignment = Alignment.Center
-//            ) {
-//                Text(text = "1", color = Color.White)
-//            }
-//            Box(
-//                modifier = Modifier
-//                    .background(Color.Green)
-//                    .size(20.dp),
-//                contentAlignment = Alignment.Center
-//            ) {
-//                Text(text = "2", color = Color.White)
-//            }
-//            Box(
-//                modifier = Modifier
-//                    .background(Color.Magenta)
-//                    .size(20.dp),
-//                contentAlignment = Alignment.Center
-//            ) {
-//                Text(text = "3", color = Color.White)
-//            }
-//        }
-        Crossfade(targetState = isDarkMode, label = "") {
+        Crossfade(targetState = isEditing, label = "") {
             when(it) {
-                true -> {
-                    Row {
-                        Box(
-                            modifier = Modifier
-                                .background(Color.Red)
-                                .size(20.dp),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(text = "1", color = Color.White)
+                false -> {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text(
+                            text = toDoData.text,
+                            modifier = Modifier.weight(1f),
+                            textAlign = TextAlign.Center,
+                            overflow = TextOverflow.Ellipsis,
+                            maxLines = 1
+                        )
+                        Text(text = "완료")
+                        Checkbox(
+                            checked = toDoData.done,
+                            onCheckedChange = { checked ->
+                                onToggle(toDoData.key, checked)
+                            }
+                        )
+
+                        Button(onClick = {
+                            isEditing = true
+                        }) {
+                            Text(text = "수정")
                         }
-                        Box(
-                            modifier = Modifier
-                                .background(Color.Green)
-                                .size(20.dp),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(text = "2", color = Color.White)
-                        }
-                        Box(
-                            modifier = Modifier
-                                .background(Color.Magenta)
-                                .size(20.dp),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(text = "3", color = Color.White)
+                        Spacer(modifier = Modifier.size(4.dp))
+                        Button(onClick = {
+                            onDelete(toDoData.key)
+                        }) {
+                            Text(text = "삭제")
                         }
                     }
                 }
-                false -> {
-                    Column {
-                        Box(
-                            modifier = Modifier
-                                .background(Color.Red)
-                                .size(20.dp),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(text = "a", color = Color.White)
-                        }
-                        Box(
-                            modifier = Modifier
-                                .background(Color.Green)
-                                .size(20.dp),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(text = "b", color = Color.White)
-                        }
-                        Box(
-                            modifier = Modifier
-                                .background(Color.Magenta)
-                                .size(20.dp),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(text = "c", color = Color.White)
+                true -> {
+                    var (newText, setNewText) = remember { mutableStateOf(toDoData.text) }
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        OutlinedTextField(
+                            value = newText,
+                            onValueChange = setNewText,
+                            modifier = Modifier.weight(1f)
+                        )
+                        Spacer(modifier = Modifier.size(4.dp))
+                        Button(onClick = {
+                            onEdit(toDoData.key, newText)
+                            isEditing = false
+                        }) {
+                            Text(text = "수정완료")
                         }
                     }
                 }
@@ -224,27 +239,28 @@ fun AnimationEx() {
 }
 
 @Composable
-fun RadioButtonWithText(
+fun ToDoInput(
     text: String,
-    color: Color = Color.Black,
-    selected: Boolean,
-    onClick: () -> Unit
+    onTextChange: (String) -> Unit,
+    onSubmit: (String) -> Unit
 ) {
     Row(
-        modifier = Modifier.selectable(
-            selected = selected,
-            onClick = onClick
-        ),
+        modifier = Modifier.padding(8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        RadioButton(
-            selected = selected,
-            onClick = onClick
+        OutlinedTextField(
+            value = text,
+            onValueChange = onTextChange,
+            modifier = Modifier.weight(1f)
         )
-        Text(
-            text = text,
-            color = color
-        )
+        Spacer(modifier = Modifier.size(8.dp))
+        Button(onClick = {
+            if (text.isNotBlank()) {
+                onSubmit(text)
+            }
+        }) {
+            Text(text = "입력")
+        }
     }
 }
 
@@ -252,19 +268,28 @@ fun RadioButtonWithText(
 @Composable
 fun DefaultPreview() {
     ComposeStudyTheme {
-        AnimationEx()
+        TopLevel()
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun RadioButtonWithTextPreview() {
+fun ToDoInputPreview() {
     ComposeStudyTheme {
-        RadioButtonWithText(
-            text = "라디오 버튼",
-            color = Color.Red,
-            selected = true,
-            onClick = {}
-        )
+        ToDoInput("테스트", {}, {})
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun ToDoPreview() {
+    ComposeStudyTheme {
+        Todo(toDoData = ToDoData(1,"테스트",true))
+    }
+}
+
+data class ToDoData(
+    val key: Int,
+    val text: String,
+    val done: Boolean = false
+)
