192a05a60433f58ed5249ee8ff1235c1f3d1d9e6
Ojongseok <whdtjr6889@naver.com>
2023-12-26 16:42:39 +0900

    [Ch 05.04] Compose 컴포넌트: CompositionLocal

---
+++ app/src/main/java/com/example/composestudy/CompositionLocalActivity.kt
@@ -0,0 +1,58 @@
+package com.example.composestudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composestudy.ui.theme.ComposeStudyTheme
+
+class CompositionLocalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            ComposeStudyTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    Card(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        CompositionLocalProvider(LocalContentColor provides Color.Magenta) {
+            Text(text = "안녕하세요")
+            Text(text = "안녕 하 세 요")
+            Text(text = "${LocalContentColor.current}")
+        }
+        Text(text = "안녕 하세요")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeStudyTheme {
+        Greeting()
+    }
+}
