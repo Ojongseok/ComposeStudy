15316e4efe3675b45a3e43d7a2c88d973c534cff
Ojongseok <whdtjr6889@naver.com>
2024-01-11 04:55:10 +0900

    [Part2. Ch 03.07] Compose 실무활용: 영화 카테고리 만들기

---
+++ app/src/main/java/com/example/composestudy/ui/components/movie/Category.kt
@@ -0,0 +1,48 @@
+package com.example.composestudy.ui.components.movie
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CategoryRow() {
+    Column {
+        CategoryTitle("Action")
+        LazyRow(
+            contentPadding = PaddingValues(horizontal = 10.dp)
+        ) {
+            item {
+                MovieItem()
+            }
+            item {
+                MovieItem()
+            }
+            item {
+                MovieItem()
+            }
+            item {
+                MovieItem()
+            }
+        }
+    }
+}
+
+@Composable
+fun CategoryTitle(title: String) {
+    Text(
+        text = "Action",
+        modifier = Modifier.padding(10.dp)
+    )
+}
+
+@Preview
+@Composable
+fun CategoryRowPreview() {
+    CategoryRow()
+}
