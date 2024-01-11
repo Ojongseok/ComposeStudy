b0ee78d9a73a822846a08c49b051d992eb250805
Ojongseok <whdtjr6889@naver.com>
2024-01-12 05:20:23 +0900

    BaseDialogPopup @Preview 제거

--- app/src/main/java/com/example/composestudy/ui/components/dialog/BaseDialogPopup.kt
+++ app/src/main/java/com/example/composestudy/ui/components/dialog/BaseDialogPopup.kt
@@ -11,16 +11,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composestudy.ui.components.dialog.components.button.DialogButtonsColumn
 import com.example.composestudy.ui.components.dialog.components.content.DialogContentWrapper
 import com.example.composestudy.ui.components.dialog.components.title.DialogTitleWrapper
 import com.example.composestudy.ui.models.dialogs.DialogButton
 import com.example.composestudy.ui.models.dialogs.DialogContent
-import com.example.composestudy.ui.models.dialogs.DialogText
 import com.example.composestudy.ui.models.dialogs.DialogTitle
-import com.example.composestudy.ui.theme.ComposeStudyTheme
 
 @Composable
 fun BaseDialogPopup(
@@ -55,60 +52,5 @@ fun BaseDialogPopup(
                 }
             }
         }
-    }
-}
-
-@Preview
-@Composable
-fun BaseDialogPopupPreview() {
-    ComposeStudyTheme {
-        BaseDialogPopup(
-            dialogTitle = DialogTitle.Header("헤더"),
-            dialogContent = DialogContent.Large(
-                DialogText.Default("디자인시스템을 엄청나게 활용중~~~디자인시스템을 엄청나게 활용중~~~디자인시스템을 엄청나게 활용중~~~")
-            ),
-            buttons = listOf(
-                DialogButton.Primary("Okay") {
-
-                }
-            )
-        )
-    }
-}
-
-@Preview
-@Composable
-fun BaseDialogPopupPreview2() {
-    ComposeStudyTheme {
-        BaseDialogPopup(
-            dialogTitle = DialogTitle.Large("라지헤더"),
-            dialogContent = DialogContent.Default(
-                DialogText.Default("디자인시스템을 엄청나게 활용중~~~디자인시스템을 엄청나게 활용중~~~디자인시스템을 엄청나게 활용중~~~")
-            ),
-            buttons = listOf(
-                DialogButton.Secondary("확인") { },
-                DialogButton.UnderlinedText("취소") { }
-            )
-        )
-    }
-}
-
-@Preview
-@Composable
-fun BaseDialogPopupPreview3() {
-    ComposeStudyTheme {
-        BaseDialogPopup(
-            dialogTitle = DialogTitle.Large("라지헤더"),
-            dialogContent = DialogContent.Rating(
-                DialogText.Rating(
-                    text = "오종석",
-                    rating = 3.5f
-                )
-            ),
-            buttons = listOf(
-                DialogButton.Primary("확인") { },
-                DialogButton.Secondary("취소") { }
-            )
-        )
     }
 }
